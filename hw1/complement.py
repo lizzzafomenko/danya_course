24411bb63a1ef69bdadf71353b8936855f857805
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2026-02-19 11:28:52 +0300

    logging

--- hw1/complement.py
+++ hw1/complement.py
@@ -1,11 +1,16 @@
 #!/Users/macbook/anaconda3/bin/python
 
 import argparse
+import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--seq')
 
 args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info("script name: {}".format(args[0]))
+logging.info("input sequence: {}".format(args.seq))
 
 nucl = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
 
@@ -18,3 +23,5 @@ for letter in args.seq:
 print(output[::-1])
 print(f'{gc/len(args.seq):.3f}')
 
+logging.info("reversed-complement sequence: {}".format(output[::-1]))
+logging.info("GC-content (input seq): {}".format(gc/len(args.seq)))
--- hw1/count_kmers.py
+++ hw1/count_kmers.py
@@ -3,6 +3,7 @@
 import argparse
 from collections import defaultdict as dd
 import json
+import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--fa')
@@ -10,6 +11,15 @@ parser.add_argument('-k', '--kmer')
 parser.add_argument('-o', '--out')
 
 args = parser.parse_args()
+
+#
+# Logging initialization
+#
+logging.basicConfig(level=logging.DEBUG)
+logging.info("script name: {}".format(args[0]))
+logging.info("input fasta file: {}".format(args.fa))
+logging.info("input kmer length: {}".format(args.kmer))
+logging.info("input output file: {}".format(args.out))
 
 out = {}
 def count_kmers(name, seq, k):
@@ -26,7 +36,7 @@ with open(args.fa, 'r') as file:
         if line[0] == '>':
             if seq != '':
                 count_kmers(name, seq, k = int(args.kmer))
-            name = line.strip().split('>')[100000]
+            name = line.strip()
             seq = ''
         else:
             seq += line.strip()
@@ -35,3 +45,5 @@ file.close()
 
 with open(args.out, 'w') as file:
     json.dump(out, file, indent=4)
+
+logging.info("done!")
